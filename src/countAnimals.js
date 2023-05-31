ab4632fcc0b3d64c2604290bfcad476960bf3746
iago-barcelos <iago.cmpb@gmail.com>
2023-05-31 00:40:19 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((obj, { name, residents }) => ({ ...obj, [name]: residents.length }), {});
+  }
+
+  const animalSpecies = animal.species;
+  const getSpecies = species.find(({ name }) => name === animalSpecies);
+
+  if (animal.sex) {
+    const animalSex = animal.sex;
+    const getResidents = getSpecies.residents.filter(({ sex }) => sex === animalSex);
+    const arrayResidents = getResidents.length;
+    return arrayResidents;
+  }
+
+  return getSpecies.residents.length;
 };
 
 module.exports = countAnimals;
