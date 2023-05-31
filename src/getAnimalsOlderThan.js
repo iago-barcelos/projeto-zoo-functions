522b8954b50affae260e6488a7d84ff71e460cbc
iago-barcelos <iago.cmpb@gmail.com>
2023-05-30 21:36:19 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+const getAnimalsOlderThan = (species, age) => {
+  const speciesFinder = data.species.find((animal) => animal.name === species);
+
+  if (!speciesFinder) {
+    return false;
+  }
+
+  const isOlderThan = !speciesFinder.residents.some((animal) => animal.age < age);
+
+  return isOlderThan;
 };
 
 module.exports = getAnimalsOlderThan;
