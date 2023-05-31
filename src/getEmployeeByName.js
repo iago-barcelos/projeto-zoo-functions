edb2fc6f83b67c8551ca0ee69eccf9e3c45a28cb
iago-barcelos <iago.cmpb@gmail.com>
2023-05-30 22:31:42 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  const { employees } = data;
+  const getEmployeeName = employees.filter(
+    (employee) => employee.firstName === employeeName || employee.lastName === employeeName,
+  );
+  return getEmployeeName[0] || {};
 };
 
 module.exports = getEmployeeByName;
