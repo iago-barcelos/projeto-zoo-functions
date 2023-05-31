5d38ce6f9d1eb03966eaeae4b81ad91c4dcf7977
iago-barcelos <iago.cmpb@gmail.com>
2023-05-31 15:40:44 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,34 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui - testando
-};
+const countEntrants = (entrants) =>
+  entrants.reduce(
+    (counts, entrant) => {
+      const { age } = entrant;
+      return {
+        child: counts.child + (age < 18 ? 1 : 0),
+        adult: counts.adult + (age >= 18 && age < 50 ? 1 : 0),
+        senior: counts.senior + (age >= 50 ? 1 : 0),
+      };
+    },
+    { child: 0, adult: 0, senior: 0 },
+  );
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || entrants.length === 0) {
+    return 0;
+  }
+
+  const prices = Object.keys(data.prices);
+
+  const total = prices.reduce((sum, category) => {
+    const count = countEntrants(entrants)[category];
+    const price = data.prices[category];
+    const categoryTotal = count * price;
+    console.log(`${category} Total:`, categoryTotal.toFixed(2));
+    return sum + categoryTotal;
+  }, 0);
+
+  return Number(total.toFixed(2));
 };
 
 module.exports = { calculateEntry, countEntrants };
