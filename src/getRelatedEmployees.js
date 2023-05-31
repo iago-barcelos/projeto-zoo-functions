8a336eac9afc7911d2efa865f6b1be40db0c7408
iago-barcelos <iago.cmpb@gmail.com>
2023-05-30 23:17:46 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const { employees } = data;
+  return employees.some((employee) => employee.managers.includes(id));
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  const { employees } = data;
+  if (isManager(managerId)) {
+    return employees.reduce((result, employee) => {
+      if (employee.managers.includes(managerId)) {
+        return [...result, `${employee.firstName} ${employee.lastName}`];
+      }
+      return result;
+    }, []);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
-
+// fazer validação com o includes
 module.exports = { isManager, getRelatedEmployees };
